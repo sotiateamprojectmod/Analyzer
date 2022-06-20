250f5fbc21f7c8c48b2bd96dd47817f4f476866a
rpenecitos <sotiateamprojects@gmail.com>
2022-06-20 12:40:31 -0400

    Removed some commented code (clean-up) -rp

--- normy.py
+++ normy.py
@@ -1,25 +1,25 @@
+from typing import List, Any
+
 import dateutil.parser
 import re
 from datetime import datetime, timedelta
 from pytz import timezone
 
+
 # Open File
-def ouvrir (timeadj):
+def ouvrir(timeadj):
     mctimepattern = '[\d]{4}-[\d]{1,2}-[\d]{1,2}\s+[\d]{1,2}:[\d]{1,2}:[\d]{1,2}[\,\.][\d]{1,3}'
     with open('mcmr31188.txt') as f:
         lines = f.readlines()
-        #print(lines)
-        mctimestamp = re.findall(mctimepattern, str(lines)) # mobicontrol log timestamp format
+        mctimestamp: list[Any] = re.findall(mctimepattern, str(lines))  # mobicontrol log timestamp format
         mcmsg = re.findall(r'\b\|.*$', str(lines))
-        #print(mctimestamp)
-        #print(mcmsg)
-        #time conversion
+        # time conversion
         for mem1 in mctimestamp:
-            oldhr = (re.findall(r'\s+[\d]{1,2}', mem1)[0]).strip()         
-            newhr = int(oldhr) + timeadj                            
-            print (re.sub(r'\s+[\d]{1,2}:', ' ' + str(newhr) + ":", str(mem1)))# + mcmsg)
-            print ('---')
-            
+            oldhr = (re.findall(r'\s+[\d]{1,2}', mem1)[0]).strip()
+            newhr = int(oldhr) + timeadj
+            print(re.sub(r'\s+[\d]{1,2}:', ' ' + str(newhr) + ":", str(mem1)))
+            print('---')
+
 
 # Dissect timestamp
 # e.g. 2021-08-30 16:34:54,560
@@ -27,16 +27,17 @@ def ouvrir (timeadj):
 
 def getDateStr():
     dtStr = "blah blah blagg 2021-08-30 16:59:28.634Not a single message has been shown on net.soti.mobicont."
-    diss2 = re.findall (r'[\d]{4}-[\d]{1,2}-[\d]{1,2}\s+[\d]{1,2}:', dtStr)
+    diss2 = re.findall(r'[\d]{4}-[\d]{1,2}-[\d]{1,2}\s+[\d]{1,2}:', dtStr)
     for dat2 in diss2:
         print(dat2)
-        
+
 
 def convertTime():
     timeFormat = '%Y-%m-%d %H:%M:%S %Z%z'
     now = datetime.now().strftime(timeFormat)
     print(now)
 
+
 ouvrir(4)
-#getDateStr()
-#convertTime()
+# getDateStr()
+# convertTime()
